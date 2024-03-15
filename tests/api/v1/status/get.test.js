aa197404468c2b5528ff0eb595359392add43eda
Hiduíno Domingos <34204904+HVDgeek@users.noreply.github.com>
2024-03-15 04:44:47 +0000

    Install `pg`

--- tests/api/v1/status/get.test.js
+++ tests/api/v1/status/get.test.js
@@ -1,6 +1,4 @@
 test("GET /api/v1/status should return status 200", async () => {
-  const response = await fetch(
-    "https://vigilant-waffle-w9vwr5qp975f9r54-3000.app.github.dev/api/v1/status",
-  );
+  const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
 });
